c369193c9f9dc6730e2e9cdbf95f19dee6a16775
AbrarHayatNadim <abrarnadim247@gmail.com>
2022-06-22 13:12:56 +0600

    Codewars

--- Isograms.py
+++ Isograms.py
@@ -1,13 +1,10 @@
 def is_isogram(string):
-
-    i=0
-    while(i < len(string)-1):
-
-        if(string[i] == string[i+1]):
-            x = False
-        else:
-            x = True
-        i +=1
-    return x    
-
-print(is_isogram("nadinm"))
+    string = string.lower()
+    for i in (string):
+        if string.count(i) > 1:
+            return False
+    return True
+    
+    
+        
+print(is_isogram("Aadi"))
